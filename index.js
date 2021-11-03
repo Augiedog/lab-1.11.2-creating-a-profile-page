1eb08b22801870fca9b4d3e091741cd0a9bf5ec9
Augie Bren <Augie>
2021-11-02 20:33:47 -0500

    done for now

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 let content = document.querySelector('.js-generated.content')
+
+let header = document.createElement('h1')
+header.setAttribute('class', 'dog-name')
+header.append('Rizzo')
+content.append(header)
+
+let pic = document.createElement('img')
+pic.setAttribute('class', 'dog-image')
+pic.setAttribute('src', './assets/rizzo.jpg')
+content.append(pic)
+
+let details = document.createElement('h3')
+details.setAttribute('class', 'dog-details')
+details.append('Description')
+content.append(details)
+
